82261d7b48320eb886a0fa313919302c494aef1e
mjl1010 <iam39470228@i06.informatica.escoladeltreball.org>
2017-09-27 18:44:53 +0200

    up

--- app/src/main/java/jmendez/escoladeltreball/org/myapplication/MainActivity.java
+++ app/src/main/java/jmendez/escoladeltreball/org/myapplication/MainActivity.java
@@ -2,15 +2,28 @@ package jmendez.escoladeltreball.org.myapplication;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    /*
-    Added by me
-     */
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                button.setTextSize(60);
+            }
+        });
+        button.setOnClickListener(view->Toast.makeText(this, "PULSADO :D", Toast.LENGTH_LONG).show());
+    }
+
+    public void btn_clickedOn() {
+
     }
 }
